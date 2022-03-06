156e30e0bc6d392c666d9fcf0bd2ce409c4616cf
Michal <noszczykmichal@wp.pl>
2022-03-06 02:23:54 +0100

    rendering projects to respective lists

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,28 @@
+//Project Type
+enum ProjectStatus {
+  Active,
+  Finished,
+}
+
+class Project {
+  id: string;
+
+  constructor(
+    public title: string,
+    public description: string,
+    public people: number,
+    public status: ProjectStatus= ProjectStatus.Active //by default every is Active
+  ) {
+    this.id = Math.random().toString();
+  }
+}
+
 // Project State Management
+type Listener = (items: Project[]) => void;
 
 class ProjectState {
-  private listeners: any[] = [];
-  private projects: any[] = [];
+  private listeners: Listener[] = [];
+  private projects: Project[] = [];
   private static instance: ProjectState;
 
   private constructor() {}
@@ -15,17 +35,13 @@ class ProjectState {
     return this.instance;
   }
 
-  addListener(listenerFn: Function) {
+  addListener(listenerFn: Listener) {
     this.listeners.push(listenerFn);
   }
 
   addProject(title: string, description: string, numOfPeople: number) {
-    const newProject = {
-      id: Math.random().toString(),
-      title: title,
-      description: description,
-      people: numOfPeople,
-    };
+    const newProject = new Project(title, description, numOfPeople);
+    // console.log(newProject)
     this.projects.push(newProject);
 
     for (const listenerFn of this.listeners) {
@@ -107,48 +123,13 @@ function AutoBind(
 
   return adjDescriptor;
 }
-// Project Class
-
-class Project {
-  projectTitle: string;
-  projectDescription: string;
-  projectPeople: number;
-  templateElement: HTMLTemplateElement;
-  hostElement: HTMLElement;
-  element: HTMLElement;
-
-  constructor(prjTitle: string, prjDesc: string, prjPeople: number) {
-    this.projectTitle = prjTitle;
-    this.projectDescription = prjDesc;
-    this.projectPeople = prjPeople;
-    this.templateElement = document.getElementById(
-      "single-project"
-    ) as HTMLTemplateElement;
-    this.hostElement = document.getElementById(
-      "active-projects-list"
-    ) as HTMLElement;
-    const importedNode = document.importNode(
-      this.templateElement.content,
-      true
-    );
-
-    this.element = importedNode.firstElementChild as HTMLElement;
-    this.createProject();
-  }
-
-  private createProject() {
-    this.element.innerText = `${this.projectTitle} +${this.projectDescription}+ ${this.projectPeople}`;
-    this.hostElement.insertAdjacentElement("afterbegin", this.element);
-  }
-}
-
 //ProjectList Class
 
 class ProjectList {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLElement;
-  assignedProjects: any[];
+  assignedProjects: Project[];
   constructor(private type: "active" | "finished") {
     this.templateElement = document.getElementById(
       "project-list"
@@ -163,8 +144,15 @@ class ProjectList {
     this.element = importedNode.firstElementChild as HTMLElement;
     this.element.id = `${this.type}-projects`;
 
-    projectState.addListener((projects: any[]) => {
-      this.assignedProjects = projects;
+    projectState.addListener((projects: Project[]) => {
+      const relevantProjects=projects.filter(prj=> {
+        if(this.type==='active'){
+          return prj.status===ProjectStatus.Active
+        }else{
+          return prj.status===ProjectStatus.Finished
+        }
+      })
+      this.assignedProjects = relevantProjects;///rendering project to respective lists
       this.renderProjects();
     });
     this.attach();
@@ -172,7 +160,9 @@ class ProjectList {
   }
 
   private renderProjects() {
-    const listEl = document.getElementById(`${this.type}-projects-list`)!;
+    const listEl = document.getElementById(
+      `${this.type}-projects-list`
+    ) as HTMLUListElement;
     listEl.innerHTML="";
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement("li");
